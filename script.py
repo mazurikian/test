02af9b7d9d72d6bdb1d9a73fc95ba41f2a27745c
Ian Mazurik <mazurikian@protonmail.com>
2025-04-02 05:44:13 -0300

    Update script.py

--- script.py
+++ script.py
@@ -5,7 +5,7 @@ from oauth2client.file import Storage
 from oauth2client.client import flow_from_clientsecrets
 from oauth2client.tools import run_flow
 
-os.system('ffmpeg -i "https://stream.kick.com/ivs/v1/196233775518/V3f9DWwe2lks/2025/3/31/3/34/LHesGrlEamEZ/media/hls/master.m3u8" -c copy output.ts')
+os.system('ffmpeg -i "https://stream.kick.com/ivs/v1/196233775518/V3f9DWwe2lks/2025/4/1/16/15/JwdvOAppHrfG/media/hls/master.m3u8" -c copy output.ts')
 
 storage = Storage("oauth2.json")
 credentials = storage.get()
@@ -20,8 +20,8 @@ request = youtube.videos().insert(
     part="snippet,status",
     body={
         "snippet": {
-            "title": "Educando La Adolfina",
-            "description": "Video subido automáticamente.",
+            "title": "(01/04/2025)",
+            "description": "HOY VENDEMOS DROGUITA RICA 🌿 + NUEVO JUEGO DE TERROR DE ASTRONAUTAS 🚀  - !duelbits !kingslv !skinclub !crew - META SUBS: 103/110 https://kick.com/vector/videos/97f4321b-9ef0-4383-b8e8-9e9ad83c6308",
             "categoryId": "22"
         },
         "status": {
